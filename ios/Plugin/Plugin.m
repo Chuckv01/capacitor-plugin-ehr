8c202aa9d40e7630e63344a2bf1036fdbb1063ff
Charles Vanderhoff <charles.vanderhoff@gmail.com>
2019-08-01 12:00:03 -0700

    Don't forget the bridging header

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -1,8 +1,8 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 
-// Define the plugin using the CAP_PLUGIN Macro, and
-// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(EhrPlugin, "EhrPlugin",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
-)
+           CAP_PLUGIN_METHOD(authorize, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(queryClinicalSampleType, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(getRequestStatusForAuthorization, CAPPluginReturnPromise);
+           )
