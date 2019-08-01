2690b675230a90cc4a02921a055065ca14905e33
Charles Vanderhoff <charles.vanderhoff@gmail.com>
2019-08-01 12:31:07 -0700

    TypeScript definations

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,53 @@
-declare module "@capacitor/core" {
+declare global {
   interface PluginRegistry {
-    EhrPlugin: EhrPluginPlugin;
+    EhrPlugin: EhrPlugin;
   }
 }
 
-export interface EhrPluginPlugin {
-  echo(options: { value: string }): Promise<{value: string}>;
+export enum ClinicalRecordAuthorization {
+  unknown = 0,
+  shouldRequest = 1,
+  unnecessary = 2,
+}
+
+export type HKClinicalSampleType =
+  | 'HKClinicalTypeIdentifierAllergyRecord'
+  | 'HKClinicalTypeIdentifierConditionRecord'
+  | 'HKClinicalTypeIdentifierImmunizationRecord'
+  | 'HKClinicalTypeIdentifierLabResultRecord'
+  | 'HKClinicalTypeIdentifierMedicationRecord'
+  | 'HKClinicalTypeIdentifierProcedureRecord'
+  | 'HKClinicalTypeIdentifierVitalSignRecord';
+
+export interface HKClinicalRecord {
+  fhirResource: FHIRResource;
+  startDate: string;
+  endDate: string;
+  uuid: string;
+  sourceURL: string;
+  metadata?: [string, any];
+  displayName: string;
+}
+
+export interface FHIRResource {
+  data: any;
+  sourceURL: string;
+  displayName: string;
+  identifier: number;
+}
+
+export interface EhrPlugin {
+  authorize(options: {
+    writePermissions: [HKClinicalSampleType],
+    readPermissions: [HKClinicalSampleType]
+  }): Promise<null>;
+
+  queryClinicalSampleType(options: {
+    sampleType: [HKClinicalSampleType]
+  }): Promise<[HKClinicalRecord]>;
+
+  getRequestStatusForAuthorization(options: {
+    writePermissions: [HKClinicalSampleType],
+    readPermissions: [HKClinicalSampleType]
+  }): Promise<ClinicalRecordAuthorization>;
 }
